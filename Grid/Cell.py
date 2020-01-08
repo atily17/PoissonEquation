fdfefdef28aceb045450664d671864414629bd83
atily17 <atilia@hotmail.co.jp>
2020-01-08 23:24:04 +0900

    Updated

--- Grid/Cell.py
+++ Grid/Cell.py
@@ -1,12 +1,40 @@
+import copy
+
 class Cell(object):
     def __init__(self, node, edge):
         self.node = node
         self.edge = edge
+        self.cells = []
 
     def generateCell(self):
         edges = self.edge.edges
-        node1 = [edge["node1"] for edge in edges ]
-        node2 = [edge["node2"] for edge in edges ]
+        checkEdges = [0 for i in range(len(edges))]
+        for i,edge in enumerate(edges):
+            if checkEdges[i] == 2:
+                continue
+            stnode = "node1"
+            ednode = "node2"
+            n1 = edge[stnode]
+            n2 = edge[ednode]
+            self.cells.append({"nodes": [n1,n2], "edges":[i]})
+            cur_edge = copy.deepcopy(edge)
+            nextedge = i
+            while(edge["node1"] != n2):
+                n1 = n2
+                nextedge = self.edge.getMinAngleEdge(nextedge, ednode)
+                cur_edge = copy.deepcopy(edges[nextedge])
+                if cur_edge["node1"] == n1:
+                    stnode = "node1"
+                    ednode = "node2"
+                elif cur_edge["node2"] == n1:
+                    stnode = "node2"
+                    ednode = "node1"
+                n2 = cur_edge[ednode]
+                self.cells[-1]["nodes"].append(n2)
+                self.cells[-1]["edges"].append(nextedge)
+
+            print(self.cells[-1])
+        
 
 
 class Triangle(Cell):
--- Grid/Edge.py
+++ Grid/Edge.py
@@ -1,19 +1,91 @@
 import numpy as np
 
 class Edge(object):
-    def __init__(self):
-        pass
-
-class Cartesian(Edge):
-    def __init__(self, domain ,node=None):
+    def __init__(self, domain, node = None):
         self.domain = domain
         if node is not None:
             self.node = node
+        self.edges = []
+
+    def setEdgeDataNode(self):
+        nodes = self.node.nodes
+        for node in nodes:
+            node["edges"] = []
+        for i, edge in enumerate(self.edges):
+            nodes[edge["node1"]]["edges"].append(i)
+            nodes[edge["node2"]]["edges"].append(i)
+        self.__test__setEdgeDataNode()
+
+    def getNextEdges(self, edge_index, node_no = None):
+        nodes = self.node.nodes
+        if node_no != "node2":
+            node1 = self.edges[edge_index]["node1"]
+            edges1 = nodes[node1]["edges"][:]
+            edges1.remove(edge_index)
+        if node_no != "node1":
+            node2 = self.edges[edge_index]["node2"]
+            edges2 = nodes[node2]["edges"][:]
+            edges2.remove(edge_index)
+        if node_no == "node1":
+            return edges1
+        if node_no == "node2":
+            return edges2
+        return edges1, edges2
+    
+    def getMinAngleEdge(self, edge_index, a_node = "node2"):
+        nodes = self.node.nodes
+        if a_node == "node2":
+            e_node = "node1"
+        elif a_node == "node1":
+            e_node = "node2"
+        v_node = self.edges[edge_index][a_node]
+        point0 = nodes[v_node]["point"]
+        point1 = nodes[self.edges[edge_index][e_node]]["point"]
+        target_vec = point1 - point0
+        nextedges_indeices = self.getNextEdges(edge_index, a_node)
+        nextnodes = []
+        for nextedges_index in nextedges_indeices:
+            if self.edges[nextedges_index]["node1"] == v_node:
+                e_node = "node2"
+            elif self.edges[nextedges_index]["node2"] == v_node:
+                e_node = "node1"
+            nextnodes.append(self.edges[nextedges_index][e_node])
+        nextpoints = np.array([nodes[nextnode]["point"] for nextnode in nextnodes])
+        next_vec = nextpoints - point0
+        angle = self.getAngle(target_vec, next_vec)
+        max_index = np.argmax(angle)
+        return nextedges_indeices[max_index]
+
+    def getAngle(self, vec1, vec2):
+        cross = vec1[0]*vec2[:,1] - vec1[1]*vec2[:,0]
+        dot = vec1[0]*vec2[:,0] + vec1[1]* vec2[:,1]
+        dot[cross < 0] += 1
+        dot[(cross == 0) & (dot < 0)] = 0
+        dot[cross > 0] -= 1
+        return dot
+
+    def __test__setEdgeDataNode(self):
+        nodes = self.node.nodes
+        debug = [0 for i in range(len(self.edges))]
+        for node in nodes:
+            for edge in node["edges"]:
+                debug[edge] += 1
+        assert(np.all(np.array(debug) == 2))
+
+
+
+
+
+
+
+class Cartesian(Edge):
+    def __init__(self, domain ,node=None):
+        super().__init__(domain, node)
+        if node is not None:
             self.generateEdge()
 
     def generateEdge(self):
         nodes = self.node.nodes
-        self.edges = []
         self.edges.extend([{"node1":i, 
                             "node2": nodes[i]["nextnode"]["f"], 
                             "position": nodes[i]["position"] if nodes[i]["position"][0] == "b" else "b" + str(nodes[i]["position"][1:])
@@ -25,7 +97,7 @@ class Cartesian(Edge):
                      for edge in self.edges]
         self._deleteCross()
         self.sort()
-        x = 1
+        self.setEdgeDataNode()
 
     def sort(self):
         self.edges = sorted(self.edges, key=lambda x: [x["node1"],x["node2"]])
